e56038f4876c754ffa80cd3a5f3d7303646220d9
Mohsen Azimi <msn.azi@gmail.com>
2015-02-23 10:25:25 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# lodash for Backbone
+
+> Exploring of possibility of using lodash for backbone to reach smaller size dependency
+
+### Warning! Don't use it, work in progress
+
+
+### License
+MIT
---
+++ index.js
@@ -0,0 +1,32 @@
+var _ = {
+  keys: require('lodash/object/keys'),
+  result: require('lodash/object/result'),
+  extend: require('lodash/object/extend'),
+  defaults: require('lodash/object/defaults'),
+  has: require('lodash/object/has'),
+  pick: require('lodash/object/pick'),
+
+  bind: require('lodash/function/bind'),
+  once: require('lodash/function/once'),
+  bindAll: require('lodash/function/bindAll'),
+
+  uniqueId: require('lodash/utility/uniqueId'),
+  matches: require('lodash/utility/matches'),
+
+  invoke: require('lodash/collection/invoke'),
+  each: require('lodash/collection/each'),
+  map: require('lodash/collection/map'),
+  any: require('lodash/collection/any'),
+
+  escape: require('lodash/string/escape'),
+
+
+  clone: require('lodash/lang/clone'),
+  isEmpty: require('lodash/lang/isEmpty'),
+  isObject: require('lodash/lang/isObject'),
+  isArray: require('lodash/lang/isArray'),
+  isString: require('lodash/lang/isString'),
+  isFunction: require('lodash/lang/isFunction'),
+  isRegExp: require('lodash/lang/isRegExp')
+
+}
