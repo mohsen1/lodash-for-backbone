a7a62fd19550ec135aa949cf7a945f95c4073aaf
Mohsen Azimi <msn.azi@gmail.com>
2015-02-23 20:12:27 -0800

    report smaller size in test

--- check-size.js
+++ check-size.js
@@ -1,7 +1,9 @@
 // Make sure final result is smaller that underscore
 
 var gzipSize = require('gzip-size');
+var prettyBytes = require('pretty-bytes');
 var fs = require('fs');
+var UNDERSCORE_SIZE = 5300;
 
 fs.readFile('./browser.min.js', function (err, fileStream) {
   if (err) {
@@ -9,8 +11,10 @@ fs.readFile('./browser.min.js', function (err, fileStream) {
   }
 
   var size = gzipSize.sync(fileStream.toString());
-  if(size >= 5300) {
-    console.log('Size is larger that underscore itself(5300). Size: ' + size);
+  if(size >= UNDERSCORE_SIZE) {
+    console.log('Size is larger that underscore itself(' + prettyBytes(UNDERSCORE_SIZE) + '). Size: ' + prettyBytes(size));
     process.exit(1);
+  } else {
+    console.log('Size is ' + prettyBytes(UNDERSCORE_SIZE - size) + ' smaller than underscore');
   }
 })
